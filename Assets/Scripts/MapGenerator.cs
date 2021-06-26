16e7161185e14f6f4aec1e97e2ad9204ba12eeaf
taylorldauphinee <59611079+taylorldauphinee@users.noreply.github.com>
2021-06-26 07:29:45 -0300

    Add files via upload
    Updated MapGenerator, added MeshGenerator

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -48,6 +48,9 @@ public class MapGenerator : MonoBehaviour
         {
             SmoothMap();
         }
+
+        MeshGenerator meshGen = GetComponent<MeshGenerator>();
+        meshGen.GenerateMesh(map, 1); // 1 is a default value
     }
 
     void RandomFillMap()
@@ -121,20 +124,20 @@ public class MapGenerator : MonoBehaviour
         return wallCount;
     }
 
-    // Debug method to visualize our map for now
-    private void OnDrawGizmos()
-    {
-        if (map != null)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                for (int y = 0; y < height; y++)
-                {
-                    Gizmos.color = (map[x, y] == 1) ? Color.black : Color.white;
-                    Vector3 position = new Vector3(-width / 2 + x + 0.5f, 0f, -height / 2 + y + 0.5f);
-                    Gizmos.DrawCube(position, Vector3.one);
-                }
-            }
-        }
-    }
+    //// Debug method to visualize our map for now
+    //private void OnDrawGizmos()
+    //{
+    //    if (map != null)
+    //    {
+    //        for (int x = 0; x < width; x++)
+    //        {
+    //            for (int y = 0; y < height; y++)
+    //            {
+    //                Gizmos.color = (map[x, y] == 1) ? Color.black : Color.white;
+    //                Vector3 position = new Vector3(-width / 2 + x + 0.5f, 0f, -height / 2 + y + 0.5f);
+    //                Gizmos.DrawCube(position, Vector3.one);
+    //            }
+    //        }
+    //    }
+    //}
 }
